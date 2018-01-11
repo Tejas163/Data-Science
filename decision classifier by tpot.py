dac7fe28ffb3f3143ebe2818c8317b459f29087b
Tejaswi SK <32892292+Tejas163@users.noreply.github.com>
2018-01-11 10:19:00 +0530

    Add files via upload

--- decision classifier by tpot.py
+++ decision classifier by tpot.py
@@ -15,7 +15,7 @@ from sklearn.tree import DecisionTreeClassifier
 from tpot.builtins import StackingEstimator
 from sklearn.metrics import accuracy_score
 # NOTE: Make sure that the class is labeled 'target' in the data file
-tpot_data = pd.read_csv('E:/Books/Data Science/project-1-blood transfusion/data/blood-transfusion_1.csv', sep=',', dtype=np.float64)
+tpot_data = pd.read_csv('<FILE_PATH>', sep=',', dtype=np.float64)
 features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
