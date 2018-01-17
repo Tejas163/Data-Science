56a96c5e2d47fdab5ad4d5b5b36daa4d839a2fa9
Tejaswi SK <32892292+Tejas163@users.noreply.github.com>
2018-01-17 11:30:14 +0530

    Add files via upload
    tools file for various purposes

---
+++ tools/dos2unix.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  9 11:02:02 2018
+
+@author: Tejas
+"""
+
+#!/usr/bin/env python
+"""
+convert dos linefeeds (crlf) to unix (lf)
+usage: dos2unix.py
+"""
+original = "word_data.pkl"
+destination = "word_data_unix.pkl"
+
+content = ''
+outsize = 0
+with open(original, 'rb') as infile:
+    content = infile.read()
+with open(destination, 'wb') as output:
+    for line in content.splitlines():
+        outsize += len(line) + 1
+        output.write(line + str.encode('\n'))
+
+print("Done. Saved %s bytes." % (len(content)-outsize))
